d71fb4605cfdf1e8b4c4f31d3455c137a6739103
remkro <krolak.remek@gmail.com>
2026-02-26 20:06:46 +0100

    project setup

---
+++ src/main/java/com/nexar/NexarRentalBeApplication.java
@@ -0,0 +1,13 @@
+package com.nexar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexarRentalBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexarRentalBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nexar/nexarrentalbe/NexarRentalBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexar.nexarrentalbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexarRentalBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
